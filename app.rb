bac91a1ade5c4b8607aa7091cd53298fdc112f6b
DahyeJung <duohui12@gmail.com>
2018-06-08 00:34:02 +0000

    sinatra-intro

---
+++ app.rb
@@ -0,0 +1,64 @@
+require 'sinatra'
+require "sinatra/reloader"
+
+
+get '/' do
+  'Helloworld! welcome'
+end
+
+
+get '/htmlfile' do
+   send_file 'views/htmlfile.html'
+end
+
+
+get '/htmltag' do
+    '<h1>html태그를 보낼 수 있습니다</h1>
+     ul>
+     <li>1</li>
+     <li>22</li>
+     <li>333</li>
+    </ul>'
+end
+
+
+get '/welcome/:name' do
+    "#{params[:name]}님 안녕하세요"
+end
+    
+    
+get '/cube/:num' do
+     a = params[:num].to_i
+     "#{a**3}"
+end
+    
+    
+get '/erbfile' do
+     @name = "dahye"
+     erb :erbfile
+end
+
+
+get '/lunch-array' do
+    #메뉴들을 배열에 저장한다
+    menu = ["냉면","피자","돈까스","김밥"];
+    #하나를 추천한다
+    @today = menu.sample
+    #erb 파일에 담아서 랜더링한다
+    erb :lunchmenu
+end
+    
+    
+    
+get '/lunch-hash' do
+    
+   menu = ["냉면","피자"]
+   hash = {"냉면" => "http://pds.joins.com/news/component/htmlphoto_mmdata/201706/05/812f0c83-b11c-47a7-a385-e7d39f48e977.jpg", 
+   "피자" => "https://pizzaalvolo.co.kr/resources/images/menu/list/10_03_1020.jpg"}
+   
+   @name = menu.sample
+   @url = hash[@name] 
+   
+   erb  :lunchmenu
+   
+end
